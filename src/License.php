3b7d7af5cc65062113dcc8e017a82f17fa161341
Sourov Chowdhury <sourovfci@gmail.com>
2021-03-26 11:02:27 +0600

    Improve license refresh functionality

--- src/License.php
+++ src/License.php
@@ -259,13 +259,13 @@ class License {
             ?>
 
             <div class="appsero-license-settings appsero-license-section">
-                <?php $this->show_license_page_card_header(); ?>
+                <?php $this->show_license_page_card_header( $license ); ?>
 
                 <div class="appsero-license-details">
                     <p>
                         <?php printf( $this->client->__trans( 'Activate <strong>%s</strong> by your license key to get professional support and automatic update from your WordPress dashboard.' ), $this->client->name ); ?>
                     </p>
-                    <form method="post" action="<?php $this->formActionUrl(); ?>" novalidate="novalidate" spellcheck="false">
+                    <form method="post" action="<?php $this->form_action_url(); ?>" novalidate="novalidate" spellcheck="false">
                         <input type="hidden" name="_action" value="<?php echo $action; ?>">
                         <input type="hidden" name="_nonce" value="<?php echo wp_create_nonce( $this->client->name ); ?>">
                         <div class="license-input-fields">
@@ -503,17 +503,21 @@ class License {
             .single-license-info p.occupied {
                 color: #E40055;
             }
-            .right-form {
+            .appsero-license-right-form {
                 margin-left: auto;
             }
-            .refresh-button {
-                padding: 5px 10px;
-                border: 1px solid #0071a1;
+            .appsero-license-refresh-button {
+                padding: 6px 10px 4px 10px;
+                border: 1px solid #0082BF;
                 border-radius: 3px;
                 margin-left: auto;
-                background: #f3f5f6;
-                color: #0071a1;
+                background-color: #0082BF;
+                color: #fff;
                 cursor: pointer;
+            }
+            .appsero-license-refresh-button .dashicons {
+                color: #fff;
+                margin-left: 0;
             }
         </style>
         <?php
@@ -574,7 +578,7 @@ class License {
     /**
      * Card header
      */
-    private function show_license_page_card_header() {
+    private function show_license_page_card_header( $license ) {
         ?>
         <div class="appsero-license-title">
             <svg enable-background="new 0 0 299.995 299.995" version="1.1" viewBox="0 0 300 300" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
@@ -583,18 +587,18 @@ class License {
                 <path d="m150 1e-3c-82.839 0-150 67.158-150 150 0 82.837 67.156 150 150 150s150-67.161 150-150c0-82.839-67.161-150-150-150zm46.09 227.12h-92.173c-9.734 0-17.626-7.892-17.626-17.629v-56.919c0-8.491 6.007-15.582 14.003-17.25v-25.697c0-27.409 22.3-49.711 49.711-49.711 27.409 0 49.709 22.3 49.709 49.711v25.697c7.993 1.673 14 8.759 14 17.25v56.919h2e-3c0 9.736-7.892 17.629-17.626 17.629z"/>
             </svg>
             <span>Activate License</span>
-        <?php if( $license && $license['key'] ) : ?>
-            <form method="post" class="right-form" action="<?php $this->formActionUrl(); ?>" novalidate="novalidate" spellcheck="false">
+
+            <?php if ( $license && $license['key'] ) : ?>
+            <form method="post" class="appsero-license-right-form" action="<?php $this->form_action_url(); ?>" novalidate="novalidate" spellcheck="false">
                 <input type="hidden" name="_action" value="refresh">
                 <input type="hidden" name="_nonce" value="<?php echo wp_create_nonce( $this->client->name ); ?>">
-                <button type="submit" name="submit" class="refresh-button">
-                    <svg style="height:16px;width:16px;" viewBox="0 0 20 20">
-                        <path d="M12.319,5.792L8.836,2.328C8.589,2.08,8.269,2.295,8.269,2.573v1.534C8.115,4.091,7.937,4.084,7.783,4.084c-2.592,0-4.7,2.097-4.7,4.676c0,1.749,0.968,3.337,2.528,4.146c0.352,0.194,0.651-0.257,0.424-0.529c-0.415-0.492-0.643-1.118-0.643-1.762c0-1.514,1.261-2.747,2.787-2.747c0.029,0,0.06,0,0.09,0.002v1.632c0,0.335,0.378,0.435,0.568,0.245l3.483-3.464C12.455,6.147,12.455,5.928,12.319,5.792 M8.938,8.67V7.554c0-0.411-0.528-0.377-0.781-0.377c-1.906,0-3.457,1.542-3.457,3.438c0,0.271,0.033,0.542,0.097,0.805C4.149,10.7,3.775,9.762,3.775,8.76c0-2.197,1.798-3.985,4.008-3.985c0.251,0,0.501,0.023,0.744,0.069c0.212,0.039,0.412-0.124,0.412-0.34v-1.1l2.646,2.633L8.938,8.67z M14.389,7.107c-0.34-0.18-0.662,0.244-0.424,0.529c0.416,0.493,0.644,1.118,0.644,1.762c0,1.515-1.272,2.747-2.798,2.747c-0.029,0-0.061,0-0.089-0.002v-1.631c0-0.354-0.382-0.419-0.558-0.246l-3.482,3.465c-0.136,0.136-0.136,0.355,0,0.49l3.482,3.465c0.189,0.186,0.568,0.096,0.568-0.245v-1.533c0.153,0.016,0.331,0.022,0.484,0.022c2.592,0,4.7-2.098,4.7-4.677C16.917,9.506,15.948,7.917,14.389,7.107 M12.217,15.238c-0.251,0-0.501-0.022-0.743-0.069c-0.212-0.039-0.411,0.125-0.411,0.341v1.101l-2.646-2.634l2.646-2.633v1.116c0,0.174,0.126,0.318,0.295,0.343c0.158,0.024,0.318,0.034,0.486,0.034c1.905,0,3.456-1.542,3.456-3.438c0-0.271-0.032-0.541-0.097-0.804c0.648,0.719,1.022,1.659,1.022,2.66C16.226,13.451,14.428,15.238,12.217,15.238"></path>
-                    </svg>
+                <button type="submit" name="submit" class="appsero-license-refresh-button">
+                    <span class="dashicons dashicons-update"></span>
                     <?php echo $this->client->__trans( 'Refresh License' ); ?>
                 </button>
             </form>
-        <?php endif; ?>
+            <?php endif; ?>
+
         </div>
         <?php
     }
@@ -765,7 +769,7 @@ class License {
     /**
      * Form action URL
      */
-    private function formActionUrl() {
+    private function form_action_url() {
         $url = add_query_arg(
             $_GET,
             admin_url( basename( $_SERVER['SCRIPT_NAME'] ) )
