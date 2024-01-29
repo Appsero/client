a360c00ff4bb284843ad8938d27b3a3a8380294a
Nurul Umbhiya <nurul-umbhiya@users.noreply.github.com>
2024-01-29 18:33:50 +0600

    fix: prevent fatal error if used with old version of Appsero Client

--- src/Client.php
+++ src/Client.php
@@ -128,6 +128,26 @@ class Client {
     }
 
     /**
+     * Initialize plugin/theme updater
+     *
+     * @return void
+     */
+    public function updater() {
+        // do not show update notice on ajax request and rest api request
+        if ( wp_doing_ajax() || ( defined( 'REST_REQUEST' ) && REST_REQUEST ) ) {
+            return;
+        }
+
+        // show deprecated notice
+        _deprecated_function( __CLASS__ . '::updater', '2.0', '\Appsero\Updater::init, for more details please visit: https://app.getwemail.io/campaigns/94eef570-bc95-448f-9178-1386f009b2d9' );
+
+        // initialize the new updater
+        if ( method_exists( '\Appsero\Updater', 'init' ) ) {
+            \Appsero\Updater::init( $this );
+        }
+    }
+
+    /**
      * Initialize license checker
      *
      * @return Appsero\License
